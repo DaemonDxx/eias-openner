4cf5166ce178d93a0751158a4c3eff676874a74b
DaemonDxx <i79535999238@gmail.com>
2022-12-28 16:33:00 +0700

    init commit

---
+++ go.mod
@@ -0,0 +1,1 @@
+module eias_openner
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"sync"
+	"time"
+)
+
+const SourceFileName = "tpl"
+
+func main() {
+
+	filepath := os.Args[1]
+	archive, err := zip.OpenReader(filepath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wg := sync.WaitGroup{}
+
+	for _, f := range archive.File {
+
+		if f.Name != SourceFileName {
+			continue
+		}
+
+		dist, err := os.Create(fmt.Sprintf("%s.xlsx", path.Base(filepath)))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		distPath := path.Join(path.Dir(filepath), dist.Name())
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		archiveFile, err := f.Open()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if _, err := io.Copy(dist, archiveFile); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := archiveFile.Close(); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := dist.Close(); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(distPath)
+		cmd := exec.Command("explorer.exe", distPath)
+
+		stdout, err := cmd.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		wg.Add(1)
+		go func() {
+			<-time.After(2 * time.Second)
+			err := os.RemoveAll(distPath)
+			if err != nil {
+				fmt.Println(err)
+			}
+			wg.Done()
+		}()
+		fmt.Printf(string(stdout))
+
+	}
+
+	wg.Wait()
+	<-time.After(10 * time.Second)
+}
